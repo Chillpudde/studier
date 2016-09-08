0557d828a97b2df9c32a8e412c04883f3c29e298
Chillpudde <ajman92@gmail.com>
2016-09-08 19:57:20 +0200

    Added uke2 in INF1000

---
+++ inf1000/uke2/MinMetode.java
@@ -0,0 +1,18 @@
+/**
+Oppgave 2.01: Min metode
+*/
+
+class MinMetode {
+    public static void main(String[] args) {
+        minMetode();
+        linjeskift();
+    }
+
+    public static void minMetode() {
+        System.out.println("Hei student");
+    }
+
+    public static void linjeskift() {
+        System.out.println("\n");
+    }
+}

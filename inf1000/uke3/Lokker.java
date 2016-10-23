3c048716b63b3767593756738e2452dc8d835365
Chillpudde <ajman92@gmail.com>
2016-10-23 17:37:44 +0200

    removed empty folder studier

---
+++ inf1000/uke3/Lokker.java
@@ -0,0 +1,10 @@
+class Lokker {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 10) {
+            x++;
+            System.out.println(x);
+        }
+    }
+}
---
+++ inf1000/uke3/Primtall.java
@@ -0,0 +1,14 @@
+// Printer ut alle primtall lavere enn N.
+
+import java.util.Scanner;
+
+class Primtall {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Skriv ut alle primtall lavere enn: ");
+        int n = Integer.parseInt(in.next());
+
+        // Primtall er delelig med seg selv og 1.
+    }
+}

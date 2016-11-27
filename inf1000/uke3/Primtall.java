bbf04846cba2efbd1f8c6388e55d3c14df53b62f
chillpudde <ajman92@gmail.com>
2016-11-27 01:07:56 +0100

    .

--- inf1000/uke3/Primtall.java
+++ inf1000/uke3/Primtall.java
@@ -10,12 +10,45 @@ class Primtall {
         int tallInn = Integer.parseInt(in.next());
 
         // Primtall er delelig med seg selv og 1.
-        int primtall = 0;
 
-        for (int i = 0; i < n; i++) {
-            if () {
-                System.out.println(i);
+        for (int i = tallInn; i != 0; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.println(i+"%"+j + " = "+ i%j);
             }
         }
     }
 }
+/*
+import java.util.Scanner;
+
+class Primtall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tall = Integer.parseInt(sc.nextLine());
+        int teller = tall;
+
+        while(teller > 1) {
+            printPrimtall(teller);
+            teller--;
+        }
+    }
+
+    public static void printPrimtall(int tall) {
+        int teller = tall-1;
+        boolean funnet = false;
+
+        System.out.println(funnet);
+        while(teller > 1) {
+            if(tall % teller == 0) {
+                System.out.println(tall + "%" + teller + "=" + tall%teller);
+                funnet = true;
+            }
+            teller--;
+        }
+
+        if(!funnet) {
+            System.out.println("Fant primtall " + tall);
+        }
+    }
+}
+*/
---
+++ inf1010/oblig1/Mus.java
@@ -0,0 +1,11 @@
+class Mus {
+    private String name;
+    private boolean state;
+
+
+    private void changeState(boolean state) {
+        if (state == true) {
+            state = false;
+        }
+    }
+}

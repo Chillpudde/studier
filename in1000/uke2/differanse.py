0ca0fc3015dfe57a8b96a12a167f4694dc9b2e08
chillpudde <ajman92@gmail.com>
2019-10-13 14:58:27 +0200

    siste_siffer.py not done

---
+++ in1000/uke2/differanse.py
@@ -0,0 +1,9 @@
+# 2.02: Differansen mellom to heltall.
+
+heltall1 = int(input())
+heltall2 = int(input())
+
+diff = heltall1 - heltall2
+
+# abs() gir absolutt verdi av argument
+print(f'Differansen mellom {heltall1} og {heltall2} er:', abs(diff))
---
+++ in1000/uke2/siste_siffer.py
@@ -0,0 +1,7 @@
+# 2.05: Finn ut om siste siffer er felles
+
+a = 46
+b = 16
+c = 551
+
+print(a%b)

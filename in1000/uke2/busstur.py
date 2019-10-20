f0fcc4dbcf42dc5d15d9e4360bdb89c391a9dc55
chillpudde <ajman92@gmail.com>
2019-10-20 04:27:58 +0200

    Ferdig med 2.11

--- in1000/uke2/busstur.py
+++ in1000/uke2/busstur.py
@@ -4,18 +4,19 @@ ant_plasser = 30
 ant_passasjerer = 0
 nye_passasjerer = 0
 stasjon = 1
-temp = 0
 
 while stasjon <= 3:
-    print(f'Stasjon {stasjon}! Hvor mange gaar paa bussen?: ', end="")
+    print(f'\nStasjon {stasjon}! Hvor mange gaar paa bussen?: ', end="")
     nye_passasjerer = int(input())
-    ant_passasjerer = ant_passasjerer + nye_passasjerer
+    ant_plasser -= nye_passasjerer
+    ant_passasjerer += nye_passasjerer
 
-    if ant_passasjerer > 30:
-        ant_plasser = ant_plasser - ant_passasjerer
-        print(f'Bussen er full. {} må gå.')
+    if ant_plasser <= 0:
+        print(f'\nBussen er full. {abs(ant_plasser)} må gå til fots.')
         break
-    else:
-        ant_passasjerer = ant_passasjerer + nye_passasjerer
-        ant_plasser = (ant_plasser - ant_passasjerer)
-        stasjon = stasjon+1
+    elif ant_plasser > 0:
+        print(f'\n{nye_passasjerer} nye passasjerer går ombord i bussen.')
+        stasjon += 1
+
+    if stasjon > 3:
+        print(f'\nBussen er fremme med {ant_passasjerer} passasjerer ombord.')

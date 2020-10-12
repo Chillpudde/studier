e40391cffe1bf93643f48e5f0c81be3cf714604f
chillpudde <ajman92@gmail.com>
2020-10-12 22:37:04 +0200

    12102020

--- in1000/host2020/uke1/oblig1/dato2.py
+++ in1000/host2020/uke1/oblig1/dato2.py
@@ -1,30 +1,41 @@
 # Oppgave 3: Problemløsning med beslutninger
 
-# 1.
-print("Skriv inn to datoer i kronologisk rekkefolge \n\n")
+"""
+ABOUT
+-------------
+Programmet tar IKKE hensyn til om man skriver inn en dato som ikke finnes.
+Det kreves av bruker at dato skrives inn nøyaktig etter formatet (DDMM) for
+at progammet skal gi et fornuftig resultat.
+For "optimal" funksjon burde følgende fikses:
+- Ta hensyn til år
+- Sjekke at formatet stemmer
+- La bruker skrive inn et "skillesymbol" (ex. DD.MM.YY)
+- Og div annet... holdt meg til oppgaves "scope"
+"""
 
-# Angir antall dager per måned
-max_dag_29 = 29
-max_dag_30 = 30
-max_dag_31 = 31
+print("| Skriv inn to datoer i kronologisk rekkefolge |\n")
 
-# Plassholder for datoene som skrives inn
-dato_1 = [0,0]
-dato_2 = [0,0]
+dato_1 = int(input("Skriv inn forste dato (DDMM): "))
+dato_2 = int(input("Skriv inn andre dato (DDMM): "))
 
-dato_1_input = input("Skriv inn 1. dato:").
+# Runder opp ved å bruke "int" (finnes mer elegante måter å gjøre det på...)
+dag_1 = (int(dato_1/100))
+dag_2 = (int(dato_2/100))
 
+# Bruker "%" operator for å hente ut tallene fra tusen- og hundredelsplassen
+mnd_1 = (dato_1%100)
+mnd_2 = (dato_2%100)
 
-print(*dato_1, sep= ".")
-print(*dato_2, sep= ".")
+"""
+I oppgaveteksten står det: ...sikre at sammenligningen i programmet ditt
+kan gjøres enklere... Usikker på hvordan jeg skal
+gjøre sammenlikningen meningsfullt "enklere", gitt de verktøyene pensumet har
+dekket frem til nå...
+"""
 
-# 2.
-# a.
-if (dato_1[0] < dato_2[0] and dato_1[1] <= dato_2[1]) or (dato_1[0] >= dato_2[0] and dato_1[1] < dato_2[1]):
+if (dag_1 < dag_2 and mnd_1 <= mnd_2) or (dag_1 >= dag_2 and mnd_1 < mnd_2):
     print("Riktig rekkefolge!")
-# b.
-elif (dato_1[1] > dato_2[1]) or(dato_1[1] == dato_2[1] and dato_1[0] > dato_2[0]):
+elif (mnd_1 > mnd_2) or (mnd_1 == mnd_2 and dag_1 > dag_2):
     print("Feil rekkefolge!")
-# c.
 else:
     print("Samme dato!")

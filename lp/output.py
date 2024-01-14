ecd1738b2084720950f158b7b61ea1870ea60f94
Chillpudde <ajman92@gmail.com>
2024-01-14 16:52:05 +0100

    Delete lp directory

--- lp/output.py
+++
@@ -1,8 +0,0 @@
-# 1.3 Assignments: Problem 4
-
-x = 4
-y = x + 1
-x = 2
-
-# When printed the output will be "2 5"
-print(x, y)
--- lp/output3.py
+++
@@ -1,7 +0,0 @@
-# 1.3 Assignments: Problem 6
-
-a, b = 2, 3
-c, b = a, c + 1
-
-# In this one c is not defined
-print(a, b, c)
